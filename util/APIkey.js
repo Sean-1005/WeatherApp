75a0368493b95c9c75ae4fdb42020593f8f44034
Sean-1005 <66577352+Sean-1005@users.noreply.github.com>
2022-04-25 23:58:07 -0400

    Create APIkey.js

---
+++ util/APIkey.js
@@ -0,0 +1,16 @@
+/***
+ * The link to the OpenWeatherAPI: https://openweathermap.org/api/one-call-api
+ * 1. Go to the OpenWeather website, create/login to your own account or 
+ * 2. Go to the dashboard and copy the API keys and repalce the value of API_KEY on line 6
+ */
+
+const API_KEY = "YOUR API KEY for OpenWeather"; 
+
+/**
+ * The link to the positionstack: https://positionstack.com/
+ * 1. Go to the website, create / login to your account
+ * 2. Go to the your dashboard, and copy and repalce the API_KEY_LOCATION with your API
+ */
+const API_KEY_LOCATION = "cefa1a78ad0e557404bab9ad526d5cd4"
+
+export {API_KEY, API_KEY_LOCATION}
